2c414cc1d40ada7075ac6286a896500f9f8acddf
sjasminetya <shaulajasminetya22@gmail.com>
2021-03-16 21:12:24 +0700

    penggunaan useEffect

--- src/function-component/arrowFunc.tsx
+++ src/function-component/arrowFunc.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 
 type Props = {
     title: string
@@ -7,6 +7,25 @@ type Props = {
 const ArrComp: FC<Props> = (props) => {
 
     const [counter, setCounter] = useState<number>(0)
+    const [user, setUser] = useState<{firstName: string, lastName: string}>({
+        firstName: '',
+        lastName: ''
+    })
+    const changeString = JSON.stringify(user)
+
+    function login() {
+        setUser({
+            firstName: 'shaula',
+            lastName: 'jasminetya'
+        })
+    }
+
+    function logout() {
+        setUser({
+            firstName: '',
+            lastName: ''
+        })
+    }
 
     function tambah(param: number): void {
         setCounter(param + 1)
@@ -16,14 +35,34 @@ const ArrComp: FC<Props> = (props) => {
         setCounter(param - 1)
     }
 
+    useEffect(() => {
+        console.log('get data')
+    }, [changeString])
+
     return (
         <div>
             <h1>{props.title}</h1>
-            <h1>{counter}</h1>
+            <h1>{counter} {user.firstName} {user.lastName}</h1>
+            {user.firstName !== '' && <DataUser />}
             <div>
+                <button onClick={login}>Login</button>
+                <button onClick={logout}>Logout</button>
                 <button onClick={() => tambah(counter)}>tambah</button>
                 <button onClick={kurang.bind(kurang, counter)}>kurang</button>
             </div>
+        </div>
+    )
+}
+
+const DataUser: FC = () => {
+    useEffect(() => {
+        return () => {
+            console.log('component ilang')
+        }
+    }, [])
+    return (
+        <div>
+            Berhasil Login
         </div>
     )
 }
